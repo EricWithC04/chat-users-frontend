54f7e860aa510ac3d2fbdc83383446cdc89bdecf
EricWithC04 <negativegame117@gmail.com>
2024-08-20 14:59:24 -0300

    feat: view errors in screen

--- src/types/register.types.d.ts
+++ src/types/register.types.d.ts
@@ -6,3 +6,12 @@ export type RegisterData = {
     password: string
     confirmPassword: string
 }
+
+export type RegisterValidations = {
+    name?: string
+    username?: string
+    description?: string
+    email?: string
+    password?: string
+    confirmPassword?: string
+}
